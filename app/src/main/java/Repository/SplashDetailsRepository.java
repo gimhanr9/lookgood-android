ed73bb8bb67096a406c89c73afaf11573d6aa9b3
Gimhan Rodrigo <gimhanrg@gmail.com>
2023-01-08 00:05:32 +0530

    changes made

--- app/src/main/java/Repository/SplashDetailsRepository.java
+++ app/src/main/java/Repository/SplashDetailsRepository.java
@@ -19,7 +19,7 @@ public class SplashDetailsRepository {
         List<OnboardingItem> items = new ArrayList<>();
         items.add(new OnboardingItem(R.drawable.ratings, "Highest Satisfaction","Our products and services has the highest customer satisfaction."));
         items.add(new OnboardingItem(R.drawable.price_tag, "Unbelievable Offers","Quality and the best price at one place."));
-        items.add(new OnboardingItem(R.drawable.express_delivery, "Fast Island-wide Delivery","Receive the product to your doorstep with 2 days."));
+        items.add(new OnboardingItem(R.drawable.express_delivery, "Fast Island-wide Delivery","Receive the product to your doorstep within 2 days."));
 
         splashDetails.setValue(items);
 
